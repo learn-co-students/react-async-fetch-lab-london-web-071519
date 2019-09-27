c3dc24715c2eb86296bf65ec7873a2b744b3ed6d
Roderick Cardenas <roderickcardenas1@gmail.com>
2019-09-27 19:22:26 +0100

    Async React

--- src/App.js
+++ src/App.js
@@ -1,1 +1,25 @@
-// create your App component here
+import React from 'react'
+
+export default class App extends React.Component {
+
+    state = {
+        peopleInSpace: []
+    }
+
+    componentDidMount() {
+        this.getPeople()
+    }
+
+    getPeople = () => {
+        fetch(`http://api.open-notify.org/astros.json`)
+        .then(resp => resp.json())
+        .then(({people}) => this.setState({ peopleInSpace: people }))
+    }
+
+    render(){
+        const { peopleInSpace } = this.state
+        return <div>
+        {peopleInSpace.map(people => <h1>{people.name}</h1>)}
+        </div>
+    }
+}
